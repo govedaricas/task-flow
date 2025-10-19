979df3c31b69c403e866216d24eb5dde6e369ae5
srdjan.govedarica <srdjan.govedarica@dwelt.net>
2025-10-19 10:37:52 +0200

    mail sending exception

--- Application/Interfaces/Implementations/EmailService.cs
+++ Application/Interfaces/Implementations/EmailService.cs
@@ -1,5 +1,7 @@
-﻿using Application.Settings;
+﻿using Application.Exceptions;
+using Application.Settings;
 using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using MimeKit;
@@ -25,12 +27,34 @@ namespace Application.Interfaces.Implementations
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart("plain") { Text = message };
 
-            using (var client = new SmtpClient())
+            try
             {
+                using var client = new SmtpClient();
+
                 await client.ConnectAsync(_smtpSettings.Server, int.Parse(_smtpSettings.Port), MailKit.Security.SecureSocketOptions.StartTls, cancellationToken);
                 await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password, cancellationToken);
                 await client.SendAsync(emailMessage, cancellationToken);
                 await client.DisconnectAsync(true, cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+            catch (SmtpCommandException ex)
+            {
+                throw new ConflictException("Email", $"SMTP command failed: {ex.Message}");
+            }
+            catch (SmtpProtocolException ex)
+            {
+                throw new ConflictException("Email", $"SMTP protocol error occurred while sending email. {ex.Message}");
+            }
+            catch (AuthenticationException ex)
+            {
+                throw new ConflictException("Email", $"SMTP authentication failed. {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new ConflictException("Email", $"Failed to send email. See logs for details. {ex.Message}");
             }
         }
     }
